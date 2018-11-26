eba1a4e200a2d986508b9fb065f4093b4d85f983
Sarah Young <hi.sarah.young@gmail.com>
2018-11-26 13:53:43 +0100

    Middlewares, Redux, Promises, APIs, Oh My git commit -am "Commiting"

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+import { API_KEY } from '../secrets';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; //TEMPLATE STRINGS
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+//returning the promise as the payload
--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/SearchBar';
+import WeatherList from '../containers/WeatherList';
 
 
 export default class App extends Component {
@@ -7,6 +8,7 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
